fd245f8385b993ff216b0cee84aa5b428be4ae7d
b6hlul <120739251+b6hlul@users.noreply.github.com>
2022-12-17 17:09:25 +0300

    a

--- Assets/Scripts/EnemyYonDegistime.cs
+++ Assets/Scripts/EnemyYonDegistime.cs
@@ -7,31 +7,13 @@ public class EnemyYonDegistime : MonoBehaviour
 {
     Vector2 temp;
 
-    public Sprite sola, saga, yukariya, asagiya;
-    void Update()
+    private void Start()
     {
-        Sprite[] gay = { sola, saga, yukariya, asagiya };
-        GetComponent<SpriteRenderer>().sprite = gay[Random r];
-
         temp = transform.position;
-
-
     }
 
-    void yatayCevir()
+    void Update()
     {
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-        Vector3 velocity = rigidbody.velocity;
-        if (velocity.x>velocity.y)
-        {
-            if (velocity.x < 0)
-            {
-                GetComponent<SpriteRenderer>().sprite = sola;
-            }
-            else if (velocity.x > 0)
-            {
-                GetComponent<SpriteRenderer>().sprite = saga;
-            }
-        }
+        temp = transform.position;
     }
 }
